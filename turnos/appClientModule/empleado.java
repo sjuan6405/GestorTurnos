e700ea123e73eaa79e55c9403af33729e7255e62
sebastian <sebastian@DESKTOP-D2E8ONE>
2024-11-20 18:05:23 -0500

    mi primer commit

---
+++ turnos/appClientModule/empleado.java
@@ -0,0 +1,24 @@
+
+public class empleado {
+
+	public String Nombre;
+	public int id;
+	private String puesto;
+	public boolean active;
+	
+	
+	public empleado(String Nombre, int id, String puesto) {
+		
+		this.Nombre=Nombre;
+		this.id=id;
+		this.puesto=puesto;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
